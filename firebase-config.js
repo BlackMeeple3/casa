e8ccc06a441192af97bfde8202288188d7354d9d
BlackMeeple3 <francescodisab@gmail.com>
2026-09-03 16:41:56 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -11,3 +11,21 @@ window.FIREBASE_CONFIG = {
   messagingSenderId: "000000000000",
   appId: "1:000000000000:web:xxxxxxxxxxxxxxxxxxxxxx"
 };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtg2pcFtebuToDdjkuY0Lxfe0zfpvQE2s",
+  authDomain: "casa-fa153.firebaseapp.com",
+  databaseURL: "https://casa-fa153-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "casa-fa153",
+  storageBucket: "casa-fa153.firebasestorage.app",
+  messagingSenderId: "430490985790",
+  appId: "1:430490985790:web:1c415dffe0fb06ea4bb86f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
